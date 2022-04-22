e84f37e681226a47036c9522217c5f07b2f1a5dd
Owen Dresner <owendrez@gmail.com>
2022-04-22 17:01:28 -0400

    commit tree 073e3621b4e87823fbe1d2a4ac4b9178151a78e6

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers=function(drivers){
+    let driver1=drivers[0]
+    let driver2=drivers[1]
+    let returndrivers= [driver1,driver2];
+    return returndrivers
+}
+const returnLastTwoDrivers=function(drivers){
+    let driver1=drivers[drivers.length-1]
+    let driver2=drivers[drivers.length-2]
+    let returndrivers= [driver2,driver1]
+    return returndrivers
+}
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(input){
+    return function(){
+        return input*input
+    }
+}
+
+//console.log(returnFirstTwoDrivers(["line1","line2","line3"]))
+    
+
+
